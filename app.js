9aee0e42b1dd5b9811863f89b006d99159ebd5b3
Paul Hoffmann <paulhoffmann410@gmail.com>
2026-07-18 14:14:34 +0200

    Enhance card display logic on DOMContentLoaded
    Added functionality to hide and show multiple cards based on user interaction.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("laubrechen-card").style.display = "none";
+    document.getElementById("rasenmähen-card").style.display = "none";
 });
 
-function laubrechen() {
-    if(document.getElementById("laubrechen-card").style.display == "none") {
-        document.getElementById("laubrechen-card").style.display = "block";
-    } else {
-        document.getElementById("laubrechen-card").style.display = "none";
-    }
+function collapse_angebote(angebot) {
+    document.getElementById("laubrechen-card").style.display = "none";
+    document.getElementById("rasenmähen-card").style.display = "none";
+    let card_id = angebot + "-card";
+    document.getElementById(card_id).style.display = "block";
 }
